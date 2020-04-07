26e6f2b851d8d4e470eb5b1fb8645a7a2eaea542
Enrique Martín Santorcaz <enriquesantorcazm@gmail.com>
2020-04-07 04:24:41 +0200

    SetterGetter Users anadidos

--- practica2/src/UserSystem/Users/User.java
+++ practica2/src/UserSystem/Users/User.java
@@ -1,14 +1,70 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package UserSystem.Users;
 
-/**
- *
- * @author crist
- */
+
 public class User {
+    private String Nombre;
+    private String Apellido1;
+    private String Apellido2;
+    private String Nick;
+    private String Email;
+    private String Contrasena;
+    private boolean Admin; 
+    private boolean EsAlumno;
+    private Penalizacion Penalizado;
+  //private ListaSuscripciones ListaSubforos
+
+
     
+    public void setNombre (String NombreUsuario){
+        Nombre = NombreUsuario;
 }
+        
+    public void setApellido1 (String Apellido1Usuario){
+        Apellido1 = Apellido1Usuario;
+}
+
+    public void setApellido2 (String Apellido2Usuario){
+        Apellido2 = Apellido2Usuario;
+}
+    
+     public void setNick (String NickUsuario){
+        Nick = NickUsuario;
+}
+     
+     public void setEmail (String EmailUsuario){
+         Email = EmailUsuario;
+}
+     
+     public void setContrasena (String ContrasenaUsuario){
+         Contrasena = ContrasenaUsuario;
+}
+     
+     public void setAdmin (boolean AdminUsuario){
+         Admin = AdminUsuario;
+}
+     
+     public void setEsAlumno (boolean EsAlumnoUsuario){
+         EsAlumno = EsAlumnoUsuario;
+}
+     
+     public void setPenalizado (Penalizacion PenalizadoUsuario){
+         Penalizado = PenalizacionUsuario;
+}
+    
+     public String getNick(){
+        return Nick;
+}
+     
+     public boolean getEsAlumno (){
+         return EsAlumno;
+}
+        
+}
+
+    
+    
+    
+
+
+        
