6f873f8da76bf30e6c622c40fa61b62bf22195d5
Aitor <aitor.o.p.00@gmail.com>
2020-04-09 13:24:01 +0200

    Serialización de la clase Ban. Echar un ojo a los métodosnuevos añadidos al final de la clase Ban

--- practica2/src/UserSystem/Ban.java
+++ practica2/src/UserSystem/Ban.java
@@ -1,14 +1,25 @@
 package UserSystem;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Date;
+import java.util.LinkedList;
 
-public class Ban{
+public class Ban implements Serializable{
     private String Nick;
     private boolean isBanned;
     private Date beginDate;
     private Date endDate;
+    private static final long serialVersionUID = 1L;
+    private File Bans = new File("DataBase/Bans/BansDataBase.obj");
 
-    public Ban(String Nick, boolean isBanned, Date beginDate, Date endDate) {
+    public Ban(final String Nick, final boolean isBanned, final Date beginDate, final Date endDate) {
         this.Nick = Nick;
         this.isBanned = isBanned;
         this.beginDate = beginDate;
@@ -19,7 +30,7 @@ public class Ban{
         return isBanned;
     }
 
-    public void setBanned(boolean isBanned) {
+    public void setBanned(final boolean isBanned) {
         this.isBanned = isBanned;
     }
 
@@ -27,7 +38,7 @@ public class Ban{
         return beginDate;
     }
 
-    public void setBeginDate(Date beginDate) {
+    public void setBeginDate(final Date beginDate) {
         this.beginDate = beginDate;
     }
 
@@ -35,7 +46,7 @@ public class Ban{
         return endDate;
     }
 
-    public void setEndDate(Date endDate) {
+    public void setEndDate(final Date endDate) {
         this.endDate = endDate;
     }
 
@@ -43,8 +54,47 @@ public class Ban{
         return Nick;
     }
 
-    public void setNick(String nick) {
+    public void setNick(final String nick) {
         Nick = nick;
     }
+
+    private LinkedList<Ban> GetBanList() throws FileNotFoundException, IOException, ClassNotFoundException{
+        FileInputStream InputFile = new FileInputStream("DataBase/Bans/BansDataBase.obj");
+        ObjectInputStream InputObject = new ObjectInputStream(InputFile);
+
+        LinkedList <Ban> BanList  = (LinkedList <Ban>) InputObject.readObject();
+
+        InputFile.close();
+        InputObject.close();
+
+        return BanList;
+    }
     
+    private void WriteDataToDataBase(Ban CreatedBan) throws IOException, ClassNotFoundException{ 
+        LinkedList <Ban> BanList  = new LinkedList();
+        //Check if there are more user´s in the file
+        try{
+            FileInputStream InputFile = new FileInputStream(Bans);
+            ObjectInputStream InputObject = new ObjectInputStream(InputFile);
+            BanList  = (LinkedList <Ban>) InputObject.readObject();
+            InputFile.close();
+            InputObject.close();
+            BanList.add(CreatedBan);
+        }
+
+        //If not this is the first user
+        catch (Exception FileNotFoundException){
+            BanList.add(CreatedBan);
+        }
+
+        //Then rewrite the list with the new user
+        FileOutputStream OutputFile = new FileOutputStream("DataBase/Users/UsersDataBase.obj");
+        ObjectOutputStream OutputObject = new ObjectOutputStream(OutputFile);
+        OutputObject.writeObject(BanList); 
+ 
+        OutputFile.close();
+        OutputObject.close();
+
+
+    }
 }
