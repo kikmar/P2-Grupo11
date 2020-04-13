d6f93415686b5fc88edd30bc0bfee9957584d937
Cette88 <sr.crnt@hotmail.com>
2020-04-13 19:08:53 +0200

    Primeras cosas de Post y Coment

--- practica2/src/ForumSystem/Coment.java
+++ practica2/src/ForumSystem/Coment.java
@@ -5,10 +5,96 @@
  */
 package ForumSystem;
 
+
+import java.util.Scanner;
+import UserSystem.Users.User;
+
+
+
 /**
  *
  * @author crist
  */
 public class Coment {
+    //ATRIBUTOS GENERALES DE LA CLASE:
+    private int assesment;
+    private String text;
+    private String user;
+    private String valorantsList;
     
+    
+    //CONSTRUCTOR DE LA CLASE:
+    public Coment (int assesment, String text, String user, String valorantsList) {
+        this.assesment = assesment;
+        this.text = text;
+        this.user = user;
+        this.valorantsList = valorantsList;
+    }
+    
+    
+    //MÉTODOS SET:
+    public void setAssesment (int valueAssesment){
+        assesment = valueAssesment;
+    }
+    
+    public void setText (String valueText){
+        text = valueText;
+    }
+    
+    public void setUser (String valueUser){
+        user = valueUser;
+    }
+    
+    public void setValorantsList (String valueValorantsList){
+        valorantsList = valueValorantsList;
+    }
+    
+    
+    
+    //MÉTODOS GET:
+    public int getAssesment(){return assesment;}
+    public String getText(){return text;}
+    public String getUser(){return user;}
+    public String getValorantsList(){return valorantsList;}
+    
+    
+    
+    //METODO PARA VER VOTACION DEL USUARIO HA VOTADO:
+    
+    public void voteComent (String Nick, boolean Value){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escriba su Nick porfavor: ");
+        Nick = sc.nextLine();
+        this.user = Nick;
+        System.out.println("¿Le ha gustado el comentario? Si/No: ");
+        String ask = sc.nextLine();
+        if (ask == "Si") {
+            Value = true;
+        } else {
+            Value = false;
+        }
+        if (Value == true){
+            this.assesment = this.assesment + 1;
+        } else {
+            this.assesment = this.assesment - 1;
+        }
+    }
+   
+   //METODO PARA CREAR UN COMENTARIO POR PARTE DEL USUARIO:
+     
+   public void createComent (String Nick, String Text){
+       System.out.println("Confirme su Nick porfavor: ");
+       Scanner sc = new Scanner(System.in);
+       Nick = sc.nextLine();
+       if (this.user != Nick){
+           System.out.println("Su nick no corresponde con el de un usuario.");
+           return;
+       }
+       else {
+           System.out.println("Escriba su comentario porfavor: ");
+           String coment = sc.nextLine();
+           this.text = coment;
+       }
+   }  
+
 }
--- practica2/src/ForumSystem/Post.java
+++ practica2/src/ForumSystem/Post.java
@@ -9,6 +9,106 @@ package ForumSystem;
  *
  * @author crist
  */
+
+import ForumSystem.PostContent.Content;
+import UserSystem.Users.User;
+
+
+/**
+ *
+ * @author crist
+ */
 public class Post {
+    //Atributos
+    private String title;
+    private Content content;
+    private int valoration;
+    private boolean visible;
+    private User creator;
+    private Coment comentList;
+    private String voteList;
+    private int updates;
     
+    
+    //Constructor de la clase
+    
+    public Post (String title, Content content, int valoration, boolean visible,User creator, Coment comentList, String voteList, int updates) {
+        this.creator = creator;
+        this.title = title;
+        this.content = content;
+        this.valoration = valoration;
+        this.visible = visible;
+        this.comentList = comentList;
+        this.voteList = voteList;
+        this.updates = updates;
+    }
+    
+    //Getters:
+    
+    public String getTitle(){
+        return title;
+    }
+    
+    public Content getContent(){
+        return content;
+    }
+    
+    public int getValoration(){
+        return valoration;
+    }
+    
+    public boolean getVisible(){
+        return visible;
+    }
+    
+    public Coment getComent(){
+        return comentList;
+    }
+    
+    public String getVoteList(){
+        return voteList;
+    }
+    
+    public int getUpdates(){
+        return updates;
+    }
+    
+    public User getCreator(){
+        return creator;
+    }
+    
+    
+    //Setters:
+    
+    public void setTitle (String title){
+        this.title = title;
+    }
+    
+    public void setContent (Content content){
+        this.content = content;
+    }
+    
+    public void setValoration (int valoration){
+        this.valoration = valoration;
+    }
+    
+    public void setVisible (boolean visible){
+        this.visible = visible;
+    }
+    
+    public void setComent (Coment comentList){
+        this.comentList = comentList;
+    }
+    
+    public void setVoteList (String voteList){
+        this.voteList = voteList;
+    }
+            
+    public void setUpdates (int updates){
+        this.updates = updates;
+    }        
+    
+    public void setCreator (User creator){
+        this.creator = creator;
+    }
 }
