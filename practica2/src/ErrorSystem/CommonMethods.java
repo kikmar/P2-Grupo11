1b25dc8b406060c17af5c61b457bef4f55b5e7ec
Aitor <aitor.o.p.00@gmail.com>
2020-04-13 20:16:20 +0200

    Clase CommondMethods codificada, echar un vistazo a ver si es correcta

--- practica2/src/ErrorSystem/CommonMethods.java
+++ practica2/src/ErrorSystem/CommonMethods.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ErrorSystem;
 
-/**
- *
- * @author crist
- */
-public class CommonMethods {
+import java.util.LinkedList;
+
+public abstract class CommonMethods {
+    LinkedList<String> ErrorList = new LinkedList<>();
     
+    public String showErrors (int ErrorIndex){
+        ErrorList.clear();
+        ErrorList.add("Error 0: Error por tratar de registrarnos con un correo no institucional.");
+        ErrorList.add("Error 1: Error por tratar de acceder a una cuenta penalizada.");
+        ErrorList.add("Error 2: Error por tratar de crear un SubForo sin los permisos necesarios.");
+        ErrorList.add("Error 3: Error por tratar de acceder al menú de administrador sin los permisos necesarios.");
+        ErrorList.add("Error 4: Error por tratar de acceder al menú de creador sin los permisos necesarios.");
+        ErrorList.add("Error 5: Error por tratar de insertar contenidos sin los permisos necesarios");
+        
+        
+        if (ErrorIndex==0){
+            return ErrorList.get(0);
+        }
+        else if (ErrorIndex==1){
+            return ErrorList.get(1);
+        }
+        else if (ErrorIndex==2){
+            return ErrorList.get(2);
+        }
+        else if (ErrorIndex==3){
+            return ErrorList.get(3);
+        }
+        else if (ErrorIndex==4){
+            return ErrorList.get(4);
+        }
+        else if (ErrorIndex==5){
+            return ErrorList.get(5);
+        }
+        else {
+            return null;
+        }
+    }
 }
