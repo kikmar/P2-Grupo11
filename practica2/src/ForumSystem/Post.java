bdad3d99b7477f89cd3a44b49fc6efb2c09e09d1
Enrique Martín Santorcaz <enriquesantorcazm@gmail.com>
2020-04-20 20:40:26 +0200

    Actualizacion post

--- practica2/src/ForumSystem/Post.java
+++ practica2/src/ForumSystem/Post.java
@@ -24,6 +24,7 @@ import java.util.Scanner;
  * @author crist
  */
 public class Post {
+
     private String Tittle;
     private int Valoration;
     private boolean Visibility;
@@ -61,225 +62,233 @@ public class Post {
     public void setUpdates(int Updates) {
         this.Updates = Updates;
     }
-    
-    private LinkedList <Content> ContentList = new LinkedList();
-    private LinkedList <Coment> ComentList = new LinkedList();
-    private LinkedList <NickVote> ValorantsList = new LinkedList();
 
-    public Post(String Tittle, String Owner,int TypeOfContent,String PostContent) throws IOException, FileNotFoundException, ClassNotFoundException {
- 
+    private LinkedList<Content> ContentList = new LinkedList();
+    private LinkedList<Coment> ComentList = new LinkedList();
+    private LinkedList<NickVote> ValorantsList = new LinkedList();
+
+    public Post(String Tittle, String Owner, int TypeOfContent, String PostContent) throws IOException, FileNotFoundException, ClassNotFoundException {
+
         boolean found = CheckIfIsUser(Owner);
         boolean isTeacher = CheckIfTeacher(Owner);
-        
-        if (found){
-           if(isTeacher){
-            
-                if (TypeOfContent==1){
+
+        if (found) {
+            if (isTeacher) {
+
+                if (TypeOfContent == 1) {
                     this.Tittle = Tittle;
                     this.Owner = Owner;
                     Content C1 = new PlainText(PostContent);
                     ContentList.add(C1);
                 }
 
-                if (TypeOfContent==2){
+                if (TypeOfContent == 2) {
                     this.Tittle = Tittle;
                     this.Owner = Owner;
                     Content C2 = new Exercises(PostContent);
                     ContentList.add(C2);
                 }
 
-                if (TypeOfContent==3){
+                if (TypeOfContent == 3) {
                     this.Tittle = Tittle;
                     this.Owner = Owner;
                     Content C3 = new Poll(PostContent);
                     ContentList.add(C3);
                 }
-            }
-        
-            else{
-                if (TypeOfContent==1){
+            } else {
+                if (TypeOfContent == 1) {
                     this.Tittle = Tittle;
                     this.Owner = Owner;
                     Content C1 = new PlainText(PostContent);
                     ContentList.add(C1);
                 }
-            } 
-        }
-        
-        
-    }
-    
-    public void MenuAdmin() throws IOException, ClassNotFoundException, ParseException{
-
-    int opcion=0;
-    
-
-    while (opcion!=3){
-        System.out.println("Menu Administrador");
-        System.out.println("1. Cambiar visibilidad del post.");
-        System.out.println("2. Penalizar a un usuario.");
-        System.out.println("3. Salir del Menu Administrador.");
-        System.out.println("Escoja una de las opciones disponibles:");
-        Scanner sc = new Scanner (System.in);
-        opcion = Integer.parseInt(sc.nextLine());
-
-        switch (opcion) {
-            case 1:
-                changeVisibility();
-                break;
-                
-            case 2:
-                Suspend();
-                break;
-                
-            case 3:
-                break;
-
-            default:
-                System.out.println("Esta opción no está disponible");
-                break;
-                
-        }
-    }
-}
-    
-    private void changeVisibility() {
-    if (isVisibility()){
-        setVisibility(false);
-    }
-    else {
-        setVisibility(true);
-    }
-}
-    
-    private void Suspend() throws IOException, ClassNotFoundException, ParseException{
-    System.out.println("Ingrese el Nick del usuario que desea penalizar:");
-    Scanner sc = new Scanner (System.in);
-    String nick = sc.nextLine();
-    System.out.println("Escriba la fecha de inicio de la penalizacion con formato DD-MM-YYYY:");
-    String fechaIni = sc.nextLine();
-    System.out.println("Escriba la fecha de fin de la penalizacion con formato DD-MM-YYYY:");
-    String fechaFin = sc.nextLine();
-
-    Ban penalizacion = new Ban (nick, true, fechaIni, fechaFin);
-}
-    
-    private boolean CheckIfTeacher(String Owner) throws IOException, FileNotFoundException, ClassNotFoundException{
-        boolean isTeacher = true;
-        
-        LinkedList UserList = GetUserList();
-        
-        for(int i=0; i<UserList.size();i++){
-            User User = (User) UserList.get(i);
-            
-            if(User.getNick().equals(Owner)){               
-                if (User.isIsStudent()){
-                   isTeacher=false; 
-                } 
             }
         }
-        
-        return isTeacher;
-        
+
     }
-    
-    private boolean CheckIfIsUser(String Owner) throws IOException, FileNotFoundException, ClassNotFoundException{
+
+    public void MenuAdmin(String nicklogeado) throws IOException, ClassNotFoundException, ParseException {
+
+        int opcion;
+        boolean exit = false;
+        boolean administrador = false;
+
+        LinkedList UserList = GetUserList();
+        for (int i = 0; i < UserList.size(); i++) {
+            User User = (User) UserList.get(i);
+
+            if (User.getNick().equals(nicklogeado)) {
+                administrador = User.isIsAdmin();
+
+            }
+        }
+        if (administrador == true) {
+            while (!exit) {
+                System.out.println(" *********************************************************** \n"
+                        + "****************** MENU ADMINISTRADOR ********************* \n"
+                        + "                                                            \n"
+                        + "               1- CAMBIAR VISIBILIDAD DEL POST              \n"
+                        + "               2- PENALIZAR USUARIO                         \n"
+                        + "               3- SALIR                                     \n"
+                        + "                                                            \n"
+                        + "************************************************************\n");
+                Scanner sc = new Scanner(System.in);
+                opcion = Integer.parseInt(sc.nextLine());
+
+                switch (opcion) {
+                    case 1:
+                        changeVisibility();
+                        break;
+
+                    case 2:
+                        Suspend();
+                        break;
+
+                    case 3:
+                        exit = true;
+                        break;
+
+                    default:
+                        System.out.println("Tipo de dato introducido no valido, introduzca una de las opciones");
+
+                }
+            }
+        }
+    }
+
+    private void changeVisibility() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Quiere cambiar la visibilidad del post? si/no");
+        String ask = sc.nextLine();
+        if (ask.equals("si")) {
+            this.Visibility = !Visibility;
+            System.out.println("Ha cambiado la visibilidad del post");
+        } else {
+            System.out.println("Se ha mantenido la visibilidad");
+
+        }
+    }
+
+    private void Suspend() throws IOException, ClassNotFoundException, ParseException {
+        System.out.println("Ingrese el Nick del usuario que desea penalizar:");
+        Scanner sc = new Scanner(System.in);
+        String nick = sc.nextLine();
+        System.out.println("Escriba la fecha de inicio de la penalizacion con formato DD-MM-YYYY:");
+        String fechaIni = sc.nextLine();
+        System.out.println("Escriba la fecha de fin de la penalizacion con formato DD-MM-YYYY:");
+        String fechaFin = sc.nextLine();
+
+        Ban penalizacion = new Ban(nick, true, fechaIni, fechaFin);
+    }
+
+    private boolean CheckIfTeacher(String Owner) throws IOException, FileNotFoundException, ClassNotFoundException {
+        boolean isTeacher = true;
+
+        LinkedList UserList = GetUserList();
+
+        for (int i = 0; i < UserList.size(); i++) {
+            User User = (User) UserList.get(i);
+
+            if (User.getNick().equals(Owner)) {
+                if (User.isIsStudent()) {
+                    isTeacher = false;
+                }
+            }
+        }
+
+        return isTeacher;
+
+    }
+
+    private boolean CheckIfIsUser(String Owner) throws IOException, FileNotFoundException, ClassNotFoundException {
         boolean found = false;
         LinkedList UserList = GetUserList();
-        
-        for(int i=0; i<UserList.size();i++){
+
+        for (int i = 0; i < UserList.size(); i++) {
             User User = (User) UserList.get(i);
-            
-            if(User.getNick().equals(Owner)){
+
+            if (User.getNick().equals(Owner)) {
                 found = true;
 
             }
         }
-        
+
         return found;
     }
-    
+
     //Returns User List from Data Base
-    private LinkedList<User> GetUserList() throws FileNotFoundException, IOException, ClassNotFoundException{
+    private LinkedList<User> GetUserList() throws FileNotFoundException, IOException, ClassNotFoundException {
         FileInputStream InputFile = new FileInputStream("DataBase/Users/UsersDataBase.obj");
         ObjectInputStream InputObject = new ObjectInputStream(InputFile);
-        
-        LinkedList <User> UserList  = (LinkedList <User>) InputObject.readObject();
-        
+
+        LinkedList<User> UserList = (LinkedList<User>) InputObject.readObject();
+
         InputFile.close();
         InputObject.close();
-        
+
         return UserList;
     }
-    
-    
-   public boolean VotePost(String Nick, boolean Vote){
+
+    public boolean VotePost(String Nick, boolean Vote) {
         boolean hasVoted = false;
         boolean isAllOk = true;
-        
-        NickVote NickVote = new NickVote(Nick,Vote);
-        
-        try{
-            
+
+        NickVote NickVote = new NickVote(Nick, Vote);
+
+        try {
+
             //Check if user has voted
-            for (int i=0; i<ValorantsList.size();i++){
-                if (ValorantsList.get(i).getNick().equals(Nick)){
-                    if (ValorantsList.get(i).isVote() == Vote){
+            for (int i = 0; i < ValorantsList.size(); i++) {
+                if (ValorantsList.get(i).getNick().equals(Nick)) {
+                    if (ValorantsList.get(i).isVote() == Vote) {
                         hasVoted = true;
                         isAllOk = false;
-                    }
-                    
-                    //If he has voted but he want to changue it
-                    else{
-                        if (Vote){
-                            Valoration += 1;                       
+                    } //If he has voted but he want to changue it
+                    else {
+                        if (Vote) {
+                            Valoration += 1;
+                        } else {
+                            Valoration -= 1;
                         }
-                        else{
-                            Valoration -=1;
-                        }
-                        
+
                         ValorantsList.remove(i);
                         ValorantsList.add(NickVote);
                     }
-                }  
+                }
             }
-            
+
             //If the user never has voted
-            if (!hasVoted){
-                if(Vote){
+            if (!hasVoted) {
+                if (Vote) {
                     Valoration += 1;
+                } else {
+                    Valoration -= 1;
                 }
-                else{
-                    Valoration -=1;                    
-                }  
                 ValorantsList.add(NickVote);
-            }    
-        } 
-        
-        //If ValoratansList is empty
-        catch(NullPointerException Npe){
-           if(ValorantsList.isEmpty()){
-                if(Vote){
+            }
+        } //If ValoratansList is empty
+        catch (NullPointerException Npe) {
+            if (ValorantsList.isEmpty()) {
+                if (Vote) {
                     Valoration += 1;
+                } else {
+                    Valoration -= 1;
                 }
-                else{
-                    Valoration -=1;                    
-                }
-                ValorantsList.add(NickVote);    
+                ValorantsList.add(NickVote);
             }
         }
 
         return isAllOk;
     }
-   
-   public String Share(){
-       return "https://uredditjc.es/"+Tittle;
-   }
-    
-    public void Mostrar(){
-        
+
+    public String Share() {
+        return "https://uredditjc.es/" + Tittle;
+    }
+
+    public void Mostrar() {
+
         System.out.println(Tittle);
-        for(int i=0; i<ContentList.size();i++){
+        for (int i = 0; i < ContentList.size(); i++) {
             System.out.println(ContentList.get(i).getPostContent());
         }
     }
@@ -287,9 +296,42 @@ public class Post {
     public int getValoration() {
         return Valoration;
     }
-    
-    
-    
-    
-    
+
+    public void menuCreator(String nicklogeado) {
+        Scanner sn = new Scanner(System.in);
+        boolean exit = false;
+        int option;
+
+        Post posts = new Post();
+        if (posts.getOwner().equals(nicklogeado)) {
+            while (!exit) {
+                System.out.println(" *********************************************************** \n"
+                        + "********************* MENU CREADOR ************************ \n"
+                        + "                                                            \n"
+                        + "               1- MODIFICAR POST                            \n"
+                        + "               2- CREAR NUEVO POST                          \n"
+                        + "               3- Salir                                     \n"
+                        + "                                                            \n"
+                        + "************************************************************\n");
+                option = sn.nextInt();
+                switch (option) {
+
+                    case 1:
+                        contentChange();
+                        break;
+                    case 2:
+                        postCreation();
+                        break;
+                    case 3:
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Tipo de dato introducido no valido, introduzca una de las opciones");
+
+                }
+
+            }
+
+        }
+    }
 }
